5fcf6bead415f710952f5471f45690a61c4bb8cd
Filipe Brandão <filipebrandao350@gmail.com>
2025-05-29 16:18:44 -0300

    Agora estou criadno a tela de login e vou fazer o backend usando o passport para criar minhas autenticações.

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,10 +3,15 @@ const router = express.Router()
 const mongoose = require("mongoose")
 require("../models/Usuario")
 const usuario = mongoose.model("usuarios")
+const bcrypt = require("bcryptjs")
 
 router.get("/registro" , (req, res) => {
     res.render("usuarios/registro")
 })
+
+//
+//  Inserindo, fazendo um hash e fazendo uma pesquisa por CPF para saber se o já tem um usuário cadastrado com os mesmo e-mail.
+//
 
 router.post("/registro", (req, res) => {
     var erros = []
@@ -35,7 +40,51 @@ router.post("/registro", (req, res) => {
 
     }else{
         
+        usuario.findOne({email: req.body.email}).lean().then((usuarioExistente) => {
+            if(usuarioExistente){
+                req.flash("erro_msg", "Já existe uma conta com este e-mail em nosso sistema.")
+                res.redirect("/usuarios/registro")
+            }
+            else{
+
+                const novoUsuario = new usuario({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: req.body.senha
+
+                })
+
+                bcrypt.genSalt(10, (erro, salt) => {
+                    bcrypt.hash(novoUsuario.senha, salt, (err, hash) => {
+                        if(erro){
+                            req.flash("erro_msg", "Houve um erro durante o salvamento do usuário.")
+                            return res.redirect("/")
+                        }
+
+                        novoUsuario.senha = hash
+
+                        console.log("Quero ver o valor do hash: " + novoUsuario.senha)
+                        novoUsuario.save().then(() => {
+                            req.flash("success_msg", "usuário criado com sucesso.")
+                            res.redirect("/")
+                        }).catch((err) => {
+                            req.flash("erro_msg", "Houve um problema interno ao criar usuario.")
+                            res.redirect("/usuarios/registro")
+                        })
+                    })
+                })
+
+            }
+        }).catch((err) => {
+            req.flash("erro_msg", "Houve um erro interno.")
+            res.redirect("/")
+        })
+
     }
+})
+
+router.get("/login", (req, res) => {
+    res.render("usuarios/login")
 })
 
 module.exports = router
